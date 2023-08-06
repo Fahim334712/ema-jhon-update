eae5c9ded9acf776195ca04a9ef7c98c67ef4aca
Fahim334712 <f.fahimcox@gmail.com>
2023-08-07 01:13:45 +0600

    simple_cart_click

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,15 +9,29 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
     return (
         <div className='shop-conatiner'>
             <div className='product-container'>
                 {
-                    products.map(product => <Product key={product.id} product={product}></Product>)
+                    products.map(product =>
+                        <Product
+                            key={product.id}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
                 }
             </div>
             <div className='order-cart'>
                 <h4>Order Summary</h4>
+                <p>Selected Item : {cart.length}</p>
             </div>
         </div>
     );
